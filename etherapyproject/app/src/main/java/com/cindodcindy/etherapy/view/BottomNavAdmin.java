15cb1f3d2daa9ffdbb353b8f73683e3fe7a02659
CindoddCindy <cindodcindy@gmail.com>
2021-06-02 21:29:51 +0700

    tambah kelas kelas

---
+++ etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/BottomNavAdmin.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.etherapy.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.etherapy.R;
+
+public class BottomNavAdmin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav_admin);
+    }
+}
