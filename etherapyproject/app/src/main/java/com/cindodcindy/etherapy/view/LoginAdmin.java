49f66b6c57be7c54952c7efc3bb68dd14b7142e9
CindoddCindy <cindodcindy@gmail.com>
2021-06-08 19:53:03 +0700

    tambah tambah

--- etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/LoginAdmin.java
+++ etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/LoginAdmin.java
@@ -6,6 +6,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cindodcindy.etherapy.R;
 import com.cindodcindy.etherapy.shared_pref.SharedPrefHandle;
@@ -16,11 +18,17 @@ public class LoginAdmin extends AppCompatActivity {
 
     private SharedPrefHandle sharedPrefHandle;
 
+    private EditText editText_nama, editText_email, editText_password;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
         sharedPrefHandle=new SharedPrefHandle(LoginAdmin.this);
+
+        editText_nama=findViewById(R.id.et_admin_nama);
+        editText_email=findViewById(R.id.et_admin_email);
+        editText_password=findViewById(R.id.et_admin_password);
 
         if (sharedPrefHandle.getSPSudahLogin()){
             startActivity(new Intent(LoginAdmin.this, BottomNavAdmin.class)
@@ -32,15 +40,25 @@ public class LoginAdmin extends AppCompatActivity {
         button_admin_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginAdmin.this, BottomNavAdmin.class);
-                startActivity(intent);
-                finish();
+                loginAdmin();
             }
         });
     }
 
     private void loginAdmin(){
 
+        String nama = editText_nama.getText().toString();
+        String email= editText_email.getText().toString();
+        String password=editText_password.getText().toString();
 
+        Toast.makeText(getApplicationContext(), "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+        sharedPrefHandle.setSpNama(SharedPrefHandle.SP_NAMA, nama);
+        sharedPrefHandle.setSpEmail(SharedPrefHandle.SP_EMAIL,email);
+        sharedPrefHandle.setSpPassword(SharedPrefHandle.SP_PASSWORD,password);
+        // Shared Pref ini berfungsi untuk menjadi trigger session login
+        sharedPrefHandle.saveSPBoolean(SharedPrefHandle.SP_SUDAH_LOGIN, true);
+        startActivity(new Intent(getApplicationContext(), BottomNavAdmin.class)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+        finish();
     }
 }
--- etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/LoginUser.java
+++ etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/LoginUser.java
@@ -6,27 +6,65 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.cindodcindy.etherapy.R;
+import com.cindodcindy.etherapy.shared_pref.SharedPrefHandle;
 
 public class LoginUser extends AppCompatActivity {
 
     private Button button_user_login;
+
+    private EditText editText_nama, editText_email, editText_password;
+
+    private SharedPrefHandle sharedPrefHandle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_user);
 
+
+        sharedPrefHandle=new SharedPrefHandle(LoginUser.this);
+
+        editText_nama=findViewById(R.id.et_user_name);
+        editText_email=findViewById(R.id.et_user_email);
+        editText_password=findViewById(R.id.et_user_password);
+
+        if (sharedPrefHandle.getSPSudahLogin()){
+            startActivity(new Intent(LoginUser.this, BottomNavAdmin.class)
+                    .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+            finish();
+        }
+
+
         button_user_login=findViewById(R.id.btn_login_user);
 
         button_user_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginUser.this, BottomNavUser.class);
-                startActivity(intent);
-                finish();
+                loginUser();
             }
         });
+    }
+
+
+    private void loginUser(){
+
+        String nama = editText_nama.getText().toString();
+        String email= editText_email.getText().toString();
+        String password=editText_password.getText().toString();
+
+        Toast.makeText(getApplicationContext(), "BERHASIL LOGIN", Toast.LENGTH_SHORT).show();
+        sharedPrefHandle.setSpNama(SharedPrefHandle.SP_NAMA, nama);
+        sharedPrefHandle.setSpEmail(SharedPrefHandle.SP_EMAIL,email);
+        sharedPrefHandle.setSpPassword(SharedPrefHandle.SP_PASSWORD,password);
+        // Shared Pref ini berfungsi untuk menjadi trigger session login
+        sharedPrefHandle.saveSPBoolean(SharedPrefHandle.SP_SUDAH_LOGIN, true);
+        startActivity(new Intent(getApplicationContext(), BottomNavUser.class)
+                .addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK));
+        finish();
     }
 }
