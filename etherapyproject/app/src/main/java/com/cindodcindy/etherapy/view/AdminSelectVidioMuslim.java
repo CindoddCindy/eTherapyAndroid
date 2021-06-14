6d9978af6b4f74b3a1e7206cecb68e89894576dd
CindoddCindy <cindodcindy@gmail.com>
2021-06-14 14:11:02 +0700

    tambah tambah layout

---
+++ etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/AdminSelectVidioMuslim.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.etherapy.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.etherapy.R;
+
+public class AdminSelectVidioMuslim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_select_vidio_muslim);
+    }
+}
---
+++ etherapyproject/app/src/main/java/com/cindodcindy/etherapy/view/UserVidioMuslim.java
@@ -0,0 +1,16 @@
+package com.cindodcindy.etherapy.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.cindodcindy.etherapy.R;
+
+public class UserVidioMuslim extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_vidio_muslim);
+    }
+}
